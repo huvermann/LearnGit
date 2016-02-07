1d35ba9a0ccac9f5399f3e89f2d53a621f064dfa
Heiko Huvermann <wp7dev@huvermann.com>
2016-02-07 16:03:24 +0100

    Property pattern to MapPosition DTO

--- SimpleGame/SimpleGame/Src/Utils/MapPosition.py
+++ SimpleGame/SimpleGame/Src/Utils/MapPosition.py
@@ -1,11 +1,27 @@
 class MapPosition(object):
-    posX = 0
-    posY = 0
-
+    """Map position data transfer object."""
+    
     def __init__(self, x, y):
-        self.posX = x
-        self.posY = y
+        self._posX = x
+        self._posY = y
         pass
+
+    @property
+    def posX(self):
+        return self._posX
+    @posX.setter
+    def posX(self, value):
+        self._posX = value
+
+    @property
+    def posY(self):
+        return self._posY
+    @posY.setter
+    def posY(self, value):
+        self._posY = value
+
+    
+
     def copy(self):
         return MapPosition(self.posX, self.posY)
 
