f1bb7bd1ba419b0f03399a08e35d57c34fa94e45
Crusader305 <huvermann@gmx.net>
2016-02-11 14:29:08 +0100

    DrawbridgeSprite Class angelegt und in Map eingefügt

--- SimpleGame/SimpleGame/Src/Sprites/SpriteFactory.py
+++ SimpleGame/SimpleGame/Src/Sprites/SpriteFactory.py
@@ -2,6 +2,7 @@ from Sprites.JimboSprite import JimboSprite
 from Sprites.CoinSprite import CoinSprite
 from Sprites.BlobSprite import BlobSprite
 from Sprites.MockSprite import MockSprite
+from Sprites.DrawbridgeSprite import DrawbridgeSprite
 
 def createSpriteInstance(spriteName, *args):
     spriteType = globals()[spriteName]
