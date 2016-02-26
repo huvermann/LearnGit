ad7d93d0e34a88521e76cf7b11320a715d7f81ac
Heiko Huvermann <wp7dev@huvermann.com>
2016-02-26 13:36:21 +0100

    renaming

--- SimpleGame/SimpleGame/Src/Plugins/PluginFactory.py
+++ SimpleGame/SimpleGame/Src/Plugins/PluginFactory.py
@@ -2,8 +2,8 @@ import importlib
 
 def createPluginInstance(pluginName):
     module_name = "Plugins.{0}".format(pluginName)
-    myClass = getattr(importlib.import_module(module_name), pluginName)
-    return myClass()
+    pluginClass = getattr(importlib.import_module(module_name), pluginName)
+    return pluginClass()
 
 
 
