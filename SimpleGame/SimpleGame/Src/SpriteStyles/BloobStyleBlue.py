8009c88a2fe6dd52f4d4f3ebc74806ba2b14b15f
Crusader305 <huvermann@gmx.net>
2016-03-04 18:36:05 +0100

    Level 2 und 3 angepasst Blob in bloob umbenannt bloobstyle red,blue,yellow

--- SimpleGame/SimpleGame/Src/SpriteStyles/BloobStyleBlue.py
+++ SimpleGame/SimpleGame/Src/SpriteStyles/BloobStyleBlue.py
@@ -1,41 +1,33 @@
 from Utils.sprites.SpriteStyleBase import SpriteStyleBase
 from Utils.Constants import AnimationNames
 
-class BlobNames(object):
-    Left = "links_und_mitte"
-    Right = "rechts_und_mitte"
-    Die = "sterben"
-    Jump = "springen"
-
-class BloobStyle(SpriteStyleBase):
+class BloobStyleBlue(SpriteStyleBase):
     """Animation style of the bloob sprite."""
     def getStyleData(self):
         result = {
             AnimationNames.Standing : {
-                "Filename": "bloob_gehen_links.png",
+                "Filename": "bloob_blue_gehen_links.png",
                 "AnimationType": "TimeBased",
                 "Delay": 25},
             AnimationNames.StandingLeft : {
-                "Filename": "bloob_gehen_links.png",
+                "Filename": "bloob_blue_gehen_links.png",
                 "AnimationType": "TimeBased",
                 "Delay": 25},
             AnimationNames.StandingRight : {
-                "Filename": "bloob_gehen_rechts.png",
+                "Filename": "bloob_blue_gehen_rechts.png",
                 "AnimationType": "TimeBased",
                 "Delay": 25},
             AnimationNames.Left : {
-                "Filename": "bloob_gehen_links.png",
+                "Filename": "bloob_blue_gehen_links.png",
                 "AnimationType": "TimeBased",
                 "Delay": 25},
             AnimationNames.Right : { 
-                "Filename": "bloob_gehen_rechts.png",
+                "Filename": "bloob_blue_gehen_rechts.png",
                 "AnimationType": "TimeBased",
                 "Delay": 25}
             
             
             }
         return result
-
-
 
 
