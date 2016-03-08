aca74bcdcd4c034828d9ab5a079e9f099a4bf759
Crusader305 <huvermann@gmx.net>
2016-03-08 13:09:08 +0100

    Teleporter Sprite und Style Datei erstellt

---
+++ SimpleGame/SimpleGame/Src/Sprites/TeleporterSprite.py
@@ -0,0 +1,26 @@
+from Utils.sprites.SpriteBase import SpriteBase, SpritePropNames
+#from Utils.UserEvents import EVENT_CHANGEVIEW
+
+class TeleporterSprite(SpriteBase):
+    """description of class"""
+    def configureFromProperties(self, properties):
+        self.name = 'Tele'
+        self._assetName = 'Teleport'
+        self.killPlayer = False
+        self.killSprite = False
+        self.points = 0
+        self.energy = 0
+        self.sound = None
+        
+        if not SpritePropNames.Style in properties:
+            self.style = self.styleFactory('TeleporterStyle', properties)
+        if not SpritePropNames.Intelligence in properties:
+            self.intelligence = self.intelligenceFactory('DefaultSpriteIntelligence', properties)
+        if not SpritePropNames.Behavior in properties:
+            self.behavior = self.behaviorFactory('TeleporterBehavior', properties)
+        if not SpritePropNames.Supplies in properties:
+            self.supplies = self.suppliesFactory('Nothing', properties)
+
+        return super().configureFromProperties(properties)
+
+
