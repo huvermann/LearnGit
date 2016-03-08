577ce26bf5fcd1cca04da53a26e91c1f9751ba2a
Crusader305 <huvermann@gmx.net>
2016-03-08 16:25:54 +0100

    -Demo Level mit TeleporterFront und Telepunkt geändert und getested -TeleporterBack Tile in Demo PNG eigefügt

--- SimpleGame/SimpleGame/Src/SpriteStyles/TeleporterFrontStyle.py
+++ SimpleGame/SimpleGame/Src/SpriteStyles/TeleporterFrontStyle.py
@@ -1,17 +1,18 @@
+class TeleporterFrontStyle(object):
+    """description of class"""
 from Utils.sprites.SpriteStyleBase import SpriteStyleBase
 from Utils.Constants import AnimationNames
 
-class TeleporterStyle(SpriteStyleBase):
+class TeleporterFrontStyle(SpriteStyleBase):
     """Animation style of the TeleporterStyle sprite."""
     def getStyleData(self):
         result = {
             AnimationNames.Standing : {
-                "Filename": "Telepunkt.png",
+                "Filename": "Teleporter_Front.png",
                 "AnimationType": "TimeBased",
                 "Delay": 100}
             }
         return result
-
 
 
 
---
+++ SimpleGame/SimpleGame/Src/Sprites/TeleporterFrontSprite.py
@@ -0,0 +1,28 @@
+class TeleporterFrontSprite(object):
+    """description of class"""
+from Utils.sprites.SpriteBase import SpriteBase, SpritePropNames
+#from Utils.UserEvents import EVENT_CHANGEVIEW
+
+class TeleporterFrontSprite(SpriteBase):
+    """description of class"""
+    def configureFromProperties(self, properties):
+        self.name = 'Tele'
+        self._assetName = 'Teleporter'
+        self.killPlayer = False
+        self.killSprite = False
+        self.points = 0
+        self.energy = 0
+        self.sound = None
+        
+        if not SpritePropNames.Style in properties:
+            self.style = self.styleFactory('TeleporterFrontStyle', properties)
+        if not SpritePropNames.Intelligence in properties:
+            self.intelligence = self.intelligenceFactory('DefaultSpriteIntelligence', properties)
+        if not SpritePropNames.Behavior in properties:
+            self.behavior = self.behaviorFactory('DefaultSpriteBehavior', properties)
+        if not SpritePropNames.Supplies in properties:
+            self.supplies = self.suppliesFactory('Nothing', properties)
+
+        return super().configureFromProperties(properties)
+
+
