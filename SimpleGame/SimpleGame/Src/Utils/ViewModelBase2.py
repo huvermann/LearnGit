90b2436d20c9e14766ae54841f85ca006c4120e0
Heiko Huvermann <wp7dev@huvermann.com>
2016-03-14 19:10:56 +0100

    Removed obsolete comments.

--- SimpleGame/SimpleGame/Src/Utils/ViewModelBase2.py
+++ SimpleGame/SimpleGame/Src/Utils/ViewModelBase2.py
@@ -30,7 +30,6 @@ class ViewModelBase2():
         self.__plugins = []
         self.__objectSprites = pygame.sprite.Group()
         self.__allSprites = pygame.sprite.Group()
-        #self.__initFont()
         self.__keyboardEventHandler = self.__initKeyboardManager()
         self.__joystickEventHandler = self.__initJoystickManager()
         self._state = ServiceLocator.getGlobalServiceInstance(ServiceNames.Gamestate)
@@ -40,13 +39,6 @@ class ViewModelBase2():
         self._lifeLostMessageCallback = None
         ServiceLocator.registerGlobalService(ServiceNames.ViewPointer, self._viewPointer)
         
-      
-
-    #def __initFont(self):
-    #    """Inits the default font."""
-    #    fontFile = getFontResourceFile("InknutAntiqua-Light")
-    #    self._font = pygame.font.Font(fontFile, 12)
-    #    pass
 
     def suspendView(self):
         """View goes into suspend mode."""
