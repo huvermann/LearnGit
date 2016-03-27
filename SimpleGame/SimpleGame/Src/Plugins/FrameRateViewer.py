f3ad16a571a776cec561f8444799f322a8914e8a
Heiko Huvermann <wp7dev@huvermann.com>
2016-03-27 10:56:28 +0200

    FrameRateViewer

--- SimpleGame/SimpleGame/Src/Plugins/FrameRateViewer.py
+++ SimpleGame/SimpleGame/Src/Plugins/FrameRateViewer.py
@@ -1,9 +1,16 @@
 from Utils.ViewPluginBase import ViewPluginBase
 from Utils.gui.FontProvider import FontProvider
+from Utils.ServiceLocator import ServiceLocator, ServiceNames
 import pygame
 
 class FrameRateViewer(ViewPluginBase):
     """description of class"""
+    def __init__(self):
+        gameState = ServiceLocator.getGlobalServiceInstance(ServiceNames.Gamestate)
+        self.clock = gameState.clock
+        
+        super().__init__()
+        self._rect = self._screen.get_rect()
 
     def drawTextLabel(self, x, y, text, font=FontProvider.defaultFont(), color=(0,0,0)):
         label = font.render(text, 1, color)
@@ -14,9 +21,8 @@ class FrameRateViewer(ViewPluginBase):
         return textPos
 
     def drawPlugin(self):
-        fps = pygame.time.Clock.get_fps()
-        pos = self.drawTextLabel(10,10, "FPS: {0}".format(fps))        
-        #return super().drawPlugin()
+        fps = self.clock.get_fps()
+        pos = self.drawTextLabel(10,self._rect.bottom - 20, "FPS: {0:.2f}".format(fps))
 
 
 
