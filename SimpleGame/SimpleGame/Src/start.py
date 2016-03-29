7a2990949b4cc633d83f83a9bcc425e723371aa6
Crusader305 <huvermann@gmx.net>
2016-03-29 18:31:10 +0200

    Schleimicon in start.py eingetragen

--- SimpleGame/SimpleGame/Src/start.py
+++ SimpleGame/SimpleGame/Src/start.py
@@ -1,7 +1,7 @@
 from Utils.TileGame import TileGame
 
 if __name__ == "__main__":
-    game = TileGame("CoolVerine", "DemoStart", "man")
+    game = TileGame("CoolVerine", "DemoStart", "schleimicon")
     game.run()
     game.cleanup()
 
